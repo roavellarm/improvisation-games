f25594b5ca37d0073c1316a934f7ab36dcb05711
Rodrigo Avellar <roavellarm@gmail.com>
2020-11-01 22:39:41 -0300

    Fix header

--- src/pages/Header/styles.ts
+++ src/pages/Header/styles.ts
@@ -1,23 +1,19 @@
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 export const Header = styled.div`
   display: flex;
+  background: rgba(0, 0, 0, 0.2);
   align-items: center;
   justify-content: space-evenly;
-  margin-bottom: 1rem;
-  padding: 1rem;
-  background: rgba(0, 0, 0, 0.1);
+  height: 3rem;
+  max-height: 3rem;
 `
 
-export const Option = styled.div`
+export const Option = styled(Link)`
   font-family: ${({ theme }) => theme.fontFamily};
   font-weight: ${({ theme }) => theme.fontWeight.regular};
-`
-
-export const Title = styled.h1`
-  font-family: ${({ theme }) => theme.fontFamily};
-  font-size: ${({ theme }) => theme.fontSize.xxl};
-  font-weight: ${({ theme }) => theme.fontWeight.bold};
-  text-align: right;
-  line-height: 3rem;
+  font-size: ${({ theme }) => theme.fontSize.sm};
+  color: ${({ theme }) => theme.colors.text};
+  text-decoration: none;
 `
