548c65a75d48735e1d48bbbd6237ebab3cc50e85
Rodrigo Avellar <roavellarm@gmail.com>
2020-11-01 22:39:17 -0300

    Update games

--- src/pages/Games/index.tsx
+++ src/pages/Games/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
-import Header from '../Header/Header'
 import * as S from './styles'
 
 export default function Games() {
   return (
-    <>
-      <Header />
-      <S.Container>
-        <S.Title>Jogo de improvisação </S.Title>
-      </S.Container>
-    </>
+    <S.Container>
+      <S.GoBack to="/">{`<--`}</S.GoBack>
+      <S.Title>Jogo de improvisação </S.Title>
+    </S.Container>
   )
 }
--- src/pages/Games/styles.ts
+++ src/pages/Games/styles.ts
@@ -1,42 +1,27 @@
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
   padding: 1.6rem;
   padding-top: 0px;
-  /* background: blue; */
   height: calc(100vh - 4.2rem);
   max-height: 100%;
   max-width: 500px;
 `
 
-export const ImageContainer = styled.div`
-  position: absolute;
-  margin-top: 2.5rem;
-  margin-left: -1.5rem;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  z-index: -999;
-  opacity: 0.3;
-`
-
-export const Image = styled.img`
-  height: auto;
-  width: 80%;
-`
-
-export const Option = styled.div`
-  font-family: ${({ theme }) => theme.fontFamily};
-  font-weight: ${({ theme }) => theme.fontWeight.regular};
-`
-
 export const Title = styled.h1`
   font-family: ${({ theme }) => theme.fontFamily};
-  font-size: ${({ theme }) => theme.fontSize.xxl};
+  font-size: ${({ theme }) => theme.fontSize.lg};
   font-weight: ${({ theme }) => theme.fontWeight.bold};
-  text-align: right;
   line-height: 3rem;
+`
+
+export const GoBack = styled(Link)`
+  text-decoration: none;
+  font-family: ${({ theme }) => theme.fontFamily};
+  font-size: ${({ theme }) => theme.fontSize.sm};
+  font-weight: ${({ theme }) => theme.fontWeight.bold};
+  color: ${({ theme }) => theme.colors.text};
 `
