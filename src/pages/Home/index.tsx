8aefd3072563f8d8e5ceb0dc74b8e868aaac281d
Rodrigo Avellar <roavellarm@gmail.com>
2020-11-01 22:40:04 -0300

    Fix home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,31 +1,18 @@
 import React from 'react'
-import Stepper from '../../components/Stepper'
-import * as S from './styles'
+import Header from '../Header'
 import backgroundImg from '../../assets/background.png'
-import Header from '../Header/Header'
+import * as S from './styles'
 
 export default function Home() {
   return (
-    <>
+    <S.Container>
       <Header />
-
-      <S.Container>
-        <S.ImageContainer>
-          <S.Image src={backgroundImg} />
-        </S.ImageContainer>
+      <S.ImageContainer>
+        <S.Image src={backgroundImg} />
+      </S.ImageContainer>
+      <S.Content>
         <S.Title>Jogos de improvisação pra violoncelistas iniciantes</S.Title>
-        {/* <Caroussel>
-        <Card title="1" image={backgroundImg} />
-        <Card title="2" image={backgroundImg} />
-        <Card title="3" image={backgroundImg} />
-        <Card title="4" image={backgroundImg} />
-        <Card title="5" image={backgroundImg} />
-        <Card title="6" image={backgroundImg} />
-        <Card title="7" image={backgroundImg} />
-        <Card title="8" image={backgroundImg} />
-      </Caroussel> */}
-        <Stepper />
-      </S.Container>
-    </>
+      </S.Content>
+    </S.Container>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,41 +2,43 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   display: flex;
+  height: 100vh;
+  width: 100vw;
+  max-height: 100%;
+  max-width: 100%;
+  flex-direction: column;
+`
+
+export const Content = styled.div`
+  margin: 0px 1rem;
+  display: flex;
+  height: 100%;
   flex-direction: column;
   justify-content: space-between;
-  padding: 1.5rem;
-  padding-top: 0px;
-  /* background: blue; */
-  height: calc(100vh - 4.2rem);
-  max-height: 100%;
-  max-width: 500px;
 `
 
 export const ImageContainer = styled.div`
   position: absolute;
-  margin-top: 2.5rem;
-  margin-left: -1.5rem;
   display: flex;
   align-items: center;
   justify-content: center;
   z-index: -999;
+  height: calc(100% - 3rem);
+  max-width: 100%;
+  margin-top: 3rem;
   opacity: 0.3;
 `
 
 export const Image = styled.img`
   height: auto;
-  width: 80%;
-`
-
-export const Option = styled.div`
-  font-family: ${({ theme }) => theme.fontFamily};
-  font-weight: ${({ theme }) => theme.fontWeight.regular};
+  width: 70%;
 `
 
 export const Title = styled.h1`
+  padding: 1.5rem 1rem;
   font-family: ${({ theme }) => theme.fontFamily};
-  font-size: ${({ theme }) => theme.fontSize.xxl};
-  font-weight: ${({ theme }) => theme.fontWeight.bold};
+  font-size: ${({ theme }) => theme.fontSize.xl};
+  font-weight: ${({ theme }) => theme.fontWeight.light};
   text-align: right;
   line-height: 3rem;
 `
