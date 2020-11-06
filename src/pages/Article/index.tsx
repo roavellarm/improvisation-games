015a338aa01bf01a02f7934c9670d6572b72d0c7
Gustavo <gustavosohne38@gmail.com>
2020-11-06 15:09:43 -0300

    ComBugsSoNoConsole

--- src/pages/Article/index.tsx
+++ src/pages/Article/index.tsx
@@ -1,41 +1,94 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-plusplus */
-import React, { useEffect, useState } from 'react'
-import ArticleObjects from '../../mocks/Articles'
+/* eslint-disable import/no-unresolved */
+import React, { useState } from 'react'
 
-interface Objects {
-  id: number
-  name: string
-  text: string
-}
+import api from '../../services/api'
 
 export default function Article() {
-  const limit = 2
-  const total = 8
-  const [pages, setPages] = useState([{}])
+  // const [products, setProducts] = useState([])
+  // const [total, setTotal] = useState(0)
+  // const [limit, setLimit] = useState(2)
+  // const [pages, setPages] = useState([])
+  // const [currentPage, setCurrentPage] = useState(1)
 
-  useEffect(() => {
-    const totalPages = total / limit
-    const arrayPages = []
+  // async function loadProducts(req, res) {
 
-    for (let i = 1; i <= totalPages; i++) {
-      arrayPages.push(i)
-    }
+  // }
 
-    setPages(arrayPages)
-  }, [limit, total])
+  async function loadProducts() {
+    const data = await api.post('/pagination')
+    // Object(setProducts(data))
 
+    console.log(data)
+
+    return data
+  }
+
+  // setTotal(10)
+  // const totalPages = Math.ceil(total / limit)
+
+  // const arrayPages = []
+  // for (let i = 1; i <= totalPages; i++) {
+  //   arrayPages.push(i)
+  // }
+
+  // setPages(arrayPages)
+
+  loadProducts()
   return (
     <div>
-      <h2>Texto de Exemplo</h2>
-      {ArticleObjects.map((objects: Objects) => {
-        return (
-          <div key={objects.id}>
-            <span>{objects.name}</span>
-            <p>{objects.text}</p>
-          </div>
-        )
-      })}
+      <h3>Tabela de produtos</h3>
+      <select>
+        <option value="5">5</option>
+        <option value="10">10</option>
+        <option value="15">15</option>
+        <option value="100">100</option>
+      </select>
+      <div>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Descrição</th>
+            <th>Preço</th>
+            <th>Imagem</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {products.map((product) => {
+            return (
+              <tr key={product.id}>
+                <td>{product.id}</td>
+                <td>{product.name}</td>
+                <td>{product.text}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </div>
+      {/* <div>
+        <div>Qtd {total}</div>
+        <button>
+          {currentPage > 1 && (
+            <button onClick={() => setCurrentPage(currentPage - 1)}>
+              Previous
+            </button>
+          )}
+          {pages.map((page) => (
+            <button
+              isSelect={page === currentPage}
+              key={page}
+              onClick={() => setCurrentPage(page)}
+            >
+              {page}
+            </button>
+          ))}
+          {currentPage < pages.length && (
+            <button onClick={() => setCurrentPage(currentPage + 1)}>
+              Next
+            </button>
+          )}
+        </button>
+      </div> */}
     </div>
   )
 }
