a0f29761128671727da57fd8d2688911f1e9c511
Rodrigo Avellar <roavellarm@gmail.com>
2020-11-08 21:24:53 -0300

    Update funcion

--- src/pages/Article/index.tsx
+++ src/pages/Article/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-array-index-key */
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import articleJS from '../../assets/article'
@@ -8,9 +9,7 @@ import ItemList from '../../components/ItemList'
 import Line from '../../components/Line'
 import Paragraph from '../../components/Paragraph'
 
-const Container = styled.div`
-  overflow-y: scroll;
-`
+const Container = styled.div``
 
 // title1(strong),
 // title2,
@@ -24,30 +23,56 @@ const Container = styled.div`
 interface Props {
   text: string
   flags: Array<string>
-  titleStrong?: string
-  rest?: string
+  titleStrong?: string | undefined
+  rest?: string | undefined
 }
 
 export default function Article() {
   const [article, setArticle] = useState<Array<Props>>([])
+  // const [counter, setCounter] = useState(0)
+  // const [pages, setPages] = useState([])
+  const MAXLENGHT = 1000
+
+  const featchPages = () => {
+    let count = 0
+    const meuArray: Array<Props> = []
+    let i = 0
+    for (i = 0; i <= articleJS.length; i += 1) {
+      if (!(articleJS[i].text.length + count >= MAXLENGHT)) {
+        count += articleJS[i].text.length
+        meuArray.push(articleJS[i])
+      }
+      break
+    }
+    return setArticle(meuArray)
+  }
 
   useEffect(() => {
-    setArticle(articleJS)
-  }, [articleJS])
+    featchPages()
+  }, [])
 
   return (
     <Container>
-      {article.map((line) => {
-        if (line.flags.includes('space')) return <br />
-        if (line.flags.includes('line')) return <Line />
-        if (line.flags.includes('title1')) return <Title1>{line.text}</Title1>
-        if (line.flags.includes('title2')) return <Title2>{line.text}</Title2>
-        if (line.flags.includes('title3')) return <Title3>{line.text}</Title3>
+      {article.map((line, index: number) => {
+        if (line.flags.includes('space'))
+          return (
+            <div key={index}>
+              <br />
+            </div>
+          )
+        if (line.flags.includes('line')) return <Line key={index} />
+        if (line.flags.includes('title1'))
+          return <Title1 key={index}>{line.text}</Title1>
+        if (line.flags.includes('title2'))
+          return <Title2 key={index}>{line.text}</Title2>
+        if (line.flags.includes('title3'))
+          return <Title3 key={index}>{line.text}</Title3>
         if (line.flags.includes('itemList'))
-          return <ItemList>{line.text}</ItemList>
+          return <ItemList key={index}>{line.text}</ItemList>
         // if (line.flags.includes('bibliography')) {
         //   return (
         //     <Biography
+        //       key={index}
         //       text={line.text}
         //       titleStrong={line.titleStrong}
         //       rest={line.rest}
@@ -55,14 +80,15 @@ export default function Article() {
         //   )
         // }
         if (line.flags.includes('citation'))
-          return <Paragraph>{line.text}</Paragraph>
-        // return <Citation>{line.text}</Citation>
+          return <Paragraph key={index}>{line.text}</Paragraph>
+        // return <Citation key={index}>{line.text}</Citation>
         if (line.flags.includes('title3Italic'))
-          return <Title3>{line.text}</Title3>
-        // return <Title3Italic>{line.text}</Title3Italic>
+          return <Title3 key={index}>{line.text}</Title3>
+        // return <Title3Italic key={index}>{line.text}</Title3Italic>
         if (line.flags.includes('paragraph')) {
           return (
             <Paragraph
+              key={index}
               bold={line.flags.includes('strong')}
               italic={line.flags.includes('italic')}
             >
