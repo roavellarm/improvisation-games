0cc69f3a51edf4def7b7b0d444d6a8c1c3281bd3
Rodrigo Avellar <roavellarm@gmail.com>
2020-11-12 20:38:23 -0300

    Updates

--- src/components/Line/index.tsx
+++ src/components/Line/index.tsx
@@ -2,29 +2,12 @@
 import React from 'react'
 import styled from 'styled-components'
 
-interface LineProps {
-  children: string
-  bold: boolean
-  italic: boolean
-}
-
-const StyledLine = styled.p<LineProps>`
-  font-style: ${({ italic }: { italic: boolean }) =>
-    italic ? 'italic' : 'normal'};
-
-  font-weight: ${({ bold }: { bold: boolean }) => (bold ? 'bold' : 'regular')};
+const StyledLine = styled.div`
+  width: 100%;
+  height: 1px;
+  background: black;
 `
 
-interface Props {
-  children: string
-  bold?: boolean
-  italic?: boolean
-}
-
-export default function Line({ children, bold, italic }: Props) {
-  return (
-    <StyledLine bold={bold || false} italic={italic || false}>
-      {children}
-    </StyledLine>
-  )
+export default function Line() {
+  return <StyledLine />
 }
