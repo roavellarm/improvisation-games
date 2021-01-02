2e0039b914345eb7103b17e73cea6c0ba14d9232
Gustavo <gustavosohne38@gmail.com>
2021-01-02 12:59:51 -0300

    asdf

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -6,7 +6,7 @@ import * as S from './styles'
 export default function Caroussel() {
   return (
     <S.Container>
-      <Carousel>
+      <Carousel itemsToShow={4}>
         <S.Card>
           <S.Title>Jogo 1</S.Title>
           <S.Image src={leaf} />
@@ -17,6 +17,38 @@ export default function Caroussel() {
         </S.Card>
         <S.Card>
           <S.Title>Jogo 3</S.Title>
+          <S.Image src={leaf} />
+        </S.Card>
+
+        <S.Card>
+          <S.Title>Jogo 4</S.Title>
+          <S.Image src={leaf} />
+        </S.Card>
+
+        <S.Card>
+          <S.Title>Jogo 5</S.Title>
+          <S.Image src={leaf} />
+        </S.Card>
+
+        <S.Card>
+          <S.Title>Jogo 6</S.Title>
+          <S.Image src={leaf} />
+        </S.Card>
+
+        <S.Card>
+          <S.Title>Jogo 7</S.Title>
+          <S.Image src={leaf} />
+        </S.Card>
+        <S.Card>
+          <S.Title>Jogo 8</S.Title>
+          <S.Image src={leaf} />
+        </S.Card>
+        <S.Card>
+          <S.Title>Jogo 9</S.Title>
+          <S.Image src={leaf} />
+        </S.Card>
+        <S.Card>
+          <S.Title>Jogo 10</S.Title>
           <S.Image src={leaf} />
         </S.Card>
       </Carousel>
