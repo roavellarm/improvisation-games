4ec78c40dec9ba77189cc3d6516d48580b31fa83
Rodrigo Avellar <roavellarm@gmail.com>
2021-01-03 06:04:46 -0300

    Update About

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
+import Navbar from '../../components/Navbar'
 
 export default function About() {
-  return <h1>Saiba mais...</h1>
+  return (
+    <>
+      <Navbar />
+      <br />
+      <br />
+      <h1 style={{ textAlign: 'center' }}>Saiba mais...</h1>
+    </>
+  )
 }
