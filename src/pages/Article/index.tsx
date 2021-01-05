2a054fe83f618cd269b0e259203aca4851e8433c
Rodrigo Avellar <roavellarm@gmail.com>
2021-01-05 19:04:58 -0300

    Start article

--- src/pages/Article/index.tsx
+++ src/pages/Article/index.tsx
@@ -1,13 +1,46 @@
 import React from 'react'
-import Navbar from '../../components/Navbar'
+import * as C from '../../components'
+import article from '../../assets/texts/article'
+import * as S from './styles'
 
 export default function Article() {
   return (
     <>
-      <Navbar />
-      <br />
-      <br />
-      <h1 style={{ textAlign: 'center' }}>Caderno de atividades</h1>
+      <C.Navbar />
+      <S.Container>
+        <S.SideArea />
+
+        <S.Content>
+          <C.HeaderTitle content="Caderno de atividades" />
+          {article.map((item, index) => {
+            if (item.flags.includes('title1'))
+              return <C.Title key={index} content={item.text} />
+
+            if (item.flags.includes('title2'))
+              return <C.SubTitle key={index} content={item.text} />
+
+            if (item.flags.includes('paragraph'))
+              return <C.Paragraph key={index} content={item.text} />
+
+            if (item.flags.includes('itemList'))
+              return <C.ItemList key={index} content={item.text} />
+
+            return <C.Paragraph key={index} content={`>>> ${item.text}`} />
+          })}
+        </S.Content>
+
+        {/* {windowWith > 900 ? ( */}
+        <S.SideArea>
+          <S.Image />
+          <C.ScrollToTopButton />
+        </S.SideArea>
+        {/* ) : (
+          <>
+            <S.Image />
+            <C.ScrollToTopButton />
+          </>
+        )} */}
+      </S.Container>
     </>
   )
 }
