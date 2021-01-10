a689b2a94c6e0d3d6a55290876adf97f1f51ac0d
Rodrigo Avellar <roavellarm@gmail.com>
2021-01-10 00:14:19 -0300

    Update about styles

--- src/pages/About/styles.ts
+++ src/pages/About/styles.ts
@@ -1,4 +1,7 @@
 import styled from 'styled-components'
+import imgTopRight from '../../assets/images/aboutTopRight.png'
+import imgBottomRight from '../../assets/images/aboutBottomRight.png'
+import imgBottomLeft from '../../assets/images/aboutBottomLeft.png'
 
 export const Container = styled.div`
   display: flex;
@@ -32,5 +35,82 @@ export const Link = styled.a`
 export const SideArea = styled.div`
   display: flex;
   flex: 1;
-  justify-content: flex-start;
+`
+
+export const ImgTopRight = styled.div`
+  background: url(${imgTopRight});
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: top right;
+
+  position: fixed;
+  z-index: 999;
+  margin-top: -56px;
+  right: 0px;
+
+  height: 170px;
+  width: 180px;
+
+  @media screen and (max-width: 1070px) {
+    width: ${`${180 * 0.7}px`};
+  }
+  @media screen and (max-width: 940px) {
+    width: ${`${180 * 0.5}px`};
+  }
+  @media screen and (max-width: 860px) {
+    width: ${`${180 * 0.34}px`};
+  }
+  @media screen and (max-width: 800px) {
+    width: ${`${180 * 0.28}px`};
+  }
+`
+
+export const ImgBottomRight = styled.div`
+  position: fixed;
+  z-index: 999;
+  bottom: 0px;
+  right: 0px;
+
+  height: 235px;
+  width: 156px;
+
+  background: url(${imgBottomRight});
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: bottom right;
+
+  @media screen and (max-width: 1070px) {
+    width: ${`${156 * 0.7}px`};
+  }
+  @media screen and (max-width: 940px) {
+    width: ${`${156 * 0.5}px`};
+  }
+  @media screen and (max-width: 860px) {
+    width: ${`${156 * 0.34}px`};
+  }
+`
+
+export const ImgBottomLeft = styled.div`
+  position: fixed;
+  z-index: 999;
+  left: 0px;
+  bottom: 0px;
+
+  height: 301px;
+  width: 146px;
+
+  background: url(${imgBottomLeft});
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: bottom left;
+
+  @media screen and (max-width: 1070px) {
+    width: ${`${146 * 0.7}px`};
+  }
+  @media screen and (max-width: 940px) {
+    width: ${`${146 * 0.5}px`};
+  }
+  @media screen and (max-width: 860px) {
+    width: ${`${146 * 0.34}px`};
+  }
 `
