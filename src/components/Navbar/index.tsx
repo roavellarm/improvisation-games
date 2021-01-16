0259a86ab39c5509a7416b2b92bad7cdf5f0e03c
Rodrigo Avellar <roavellarm@gmail.com>
2021-01-16 20:03:48 -0300

    Update navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,29 +1,42 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import gameList from '../../assets/texts/games'
+import { gameOptions } from '../../helpers/game'
+import { anchors } from '../../helpers/article'
 import CarousselNavbar from '../CarouselNavbar'
+import Dropdown from '../Drodown'
 import * as S from './styles'
 
 export type NavbarProps = {
   isGamePage?: boolean
+  isArticlePage?: boolean
 }
 
-export default function Navbar({ isGamePage }: NavbarProps) {
+export default function Navbar({ isGamePage, isArticlePage }: NavbarProps) {
   const { push } = useHistory()
   const [windowWith, setWindowWidth] = useState(window.innerWidth)
+  const [windowHeight, setWindowHeight] = useState(window.innerHeight)
 
-  const handleGamePage = useMemo(() => {
-    return isGamePage && windowWith > 700
-  }, [isGamePage, windowWith])
+  const renderGamesOptions = () => {
+    if (windowWith > 700) return <CarousselNavbar gameList={gameList} />
+    return <Dropdown title="Jogos" options={gameOptions} />
+  }
 
-  const updateWindowWidth = useCallback(() => {
+  const renderArticleOptions = () => {
+    if (windowWith <= 800 || windowHeight <= 645)
+      return <Dropdown title="Tópicos" isArticlePage options={anchors} />
+    return undefined
+  }
+
+  const updateWindowSize = useCallback(() => {
     setWindowWidth(window.innerWidth)
+    setWindowHeight(window.innerHeight)
   }, [])
 
   useEffect(() => {
-    window.addEventListener('resize', updateWindowWidth)
-    return () => window.removeEventListener('resize', updateWindowWidth)
-  }, [updateWindowWidth])
+    window.addEventListener('resize', updateWindowSize)
+    return () => window.removeEventListener('resize', updateWindowSize)
+  }, [updateWindowSize])
 
   return (
     <S.StyledNavbar>
@@ -31,14 +44,12 @@ export default function Navbar({ isGamePage }: NavbarProps) {
       <S.Container isGamePage={isGamePage}>
         <S.SideArea onClick={() => push('/')}>{`<- Voltar`}</S.SideArea>
         <S.Spacer>
-          {handleGamePage && <CarousselNavbar gameList={gameList} />}
+          {isGamePage
+            ? renderGamesOptions()
+            : isArticlePage && renderArticleOptions()}
         </S.Spacer>
         <S.SideArea />
       </S.Container>
     </S.StyledNavbar>
   )
-}
-
-Navbar.defaultProps = {
-  isGamePage: false,
 }
