5374f8c049d316aaa4def34ea28d5b37468cd238
Rodrigo Avellar <roavellarm@gmail.com>
2021-01-16 20:02:17 -0300

    Update line

--- src/components/Line/styles.ts
+++ src/components/Line/styles.ts
@@ -4,8 +4,7 @@ export const StyledLine = styled.div`
   display: flex;
   flex-direction: row;
   height: 1px;
-  opacity: 0.5;
-  background: ${({ theme }) => theme.colors.greenLight};
+  background: rgba(91, 124, 107, 0.5);
   box-shadow: 1px 1px 2px ${({ theme }) => theme.colors.greenLight};
   border-radius: 8px;
   margin: 4rem -0.8rem;
