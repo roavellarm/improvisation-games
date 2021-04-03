845aed9d6850ab7cd6f3bdfde9cd0e3b19703965
Rodrigo Avellar <roavellarm@gmail.com>
2021-04-02 23:48:24 -0300

    Add loadable components to exports

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import loadable from 'loadable-components'
+
+export const About = loadable(() => import('./About'))
+export const Article = loadable(() => import('./Article'))
+export const Game = loadable(() => import('./Game'))
+export const Home = loadable(() => import('./Home'))
+export const NotFound = loadable(() => import('./NotFound'))
