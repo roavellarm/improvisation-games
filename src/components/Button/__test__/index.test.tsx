471d0d615e563489b8d41f3c5df8abf6f63afc0c
Rodrigo Avellar <roavellarm@gmail.com>
2021-04-03 01:27:58 -0300

    Update Button test

--- src/components/Button/__test__/index.test.tsx
+++ src/components/Button/__test__/index.test.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
+import 'jest-styled-components'
 
+import { withTheme } from 'config/testSetup'
 import Button from '..'
-import { setTestComponent } from '../../../config/testSetup'
 
 describe('Button', () => {
   it('should render correctly', () => {
     const { queryByText } = render(
-      setTestComponent(<Button title="Foo bar" onClick={jest.fn()} />)
+      withTheme(<Button title="Foo bar" onClick={jest.fn()} />)
     )
     expect(queryByText('Foo bar')).toBeInTheDocument()
   })
@@ -18,11 +19,33 @@ describe('when the button is clicked', () => {
   it('should call the function', () => {
     const testFunction = jest.fn()
     const { queryByText } = render(
-      setTestComponent(
-        <Button title="Foo bar" onClick={() => testFunction()} />
-      )
+      withTheme(<Button title="Foo bar" onClick={() => testFunction()} />)
     )
     fireEvent.click(queryByText('Foo bar') as Element)
     expect(testFunction).toHaveBeenCalled()
+  })
+})
+
+describe('when the mouse is over the button', () => {
+  it('should change the image position', () => {
+    const { queryByText, queryByAltText } = render(
+      withTheme(<Button title="Foo bar" onClick={jest.fn()} />)
+    )
+    const element = queryByText('Foo bar') as Element
+    fireEvent.mouseEnter(element)
+    const image = queryByAltText('leaf')
+    expect(image).toHaveStyleRule('transform', 'rotate(90deg)')
+  })
+})
+
+describe('when the mouse leaves off the button', () => {
+  it('should change the image to the default position', () => {
+    const { queryByText, queryByAltText } = render(
+      withTheme(<Button title="Foo bar" onClick={jest.fn()} />)
+    )
+    const element = queryByText('Foo bar') as Element
+    fireEvent.mouseLeave(element)
+    const image = queryByAltText('leaf')
+    expect(image).toHaveStyleRule('transform', undefined)
   })
 })
