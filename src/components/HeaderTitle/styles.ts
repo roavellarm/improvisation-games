348ce04b728cfb8120fa5373349820cc755029f7
Gustavo <gustavo.prog40@gmail.com>
2021-04-09 14:13:30 -0300

    headerTitle

--- src/components/HeaderTitle/styles.ts
+++ src/components/HeaderTitle/styles.ts
@@ -6,10 +6,12 @@ export const StyledHeaderTitle = styled.p<Props>`
   color: ${({ theme, isAboutPage }) =>
     isAboutPage ? theme.colors.greenDark : theme.colors.black};
   margin-bottom: ${({ isAboutPage }) => !isAboutPage && '3rem'};
+  text-align: ${({ isAboutPage }) => (isAboutPage ? 'center' : 'left')};
 
   @media screen and (max-width: 1000px) {
     font-size: 2.625rem;
-    text-align: center;
+    text-align: ${({ isAboutPage }) => (isAboutPage ? 'left' : 'left')};
+
     margin-bottom: 2rem;
   }
 `
