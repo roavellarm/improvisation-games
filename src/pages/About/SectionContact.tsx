18196a4dba5099a40980d611ad51cd8414b111f1
Gustavo Fernando Sohne <51864014+GustavoFSProg@users.noreply.github.com>
2021-04-14 22:55:23 -0300

    Md text - It was Created a Link to readme.md on repo (#20)
    * feito
    
    * readme
    
    * Update contacts
    
    Co-authored-by: Rodrigo Avellar <18143403+roavellarm@users.noreply.github.com>
    Co-authored-by: Rodrigo Avellar <roavellarm@gmail.com>

--- src/pages/About/SectionContact.tsx
+++ src/pages/About/SectionContact.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import loadable from 'loadable-components'
-import contact from 'helpers/contact'
+import contacts from 'helpers/contacts'
 import { Link } from './styles'
 
 const HeaderTitle = loadable(() => import('components/HeaderTitle'))
 const Paragraph = loadable(() => import('components/Paragraph'))
 const Title = loadable(() => import('components/Title'))
+const ItemList = loadable(() => import('components/ItemList'))
 
 export default function SectionContact() {
-  const { email, subject, body } = contact
+  const { gustavo, marta, rodrigo } = contacts
+  const subject = 'Contato jogoscello.com'
+  const body = 'Olá! Eu sou <seu nome>...'
 
   return (
     <>
@@ -20,10 +23,32 @@ export default function SectionContact() {
 
       <Title>
         {`Entre em contato `}
-        <Link target="_blank" href={`mailto:${email}?subject=${subject}&body=${body}`}>
+        <Link target="_blank" href={`mailto:${marta}?subject=${subject}&body=${body}`}>
           aqui
         </Link>
       </Title>
+
+      <HeaderTitle isAboutPage>Colaboradores</HeaderTitle>
+      <ItemList>
+        {`Desenhos: `}
+        <Link target="_blank" href="https://www.samantafloor.com.br/">
+          Samanta Flôor
+        </Link>
+      </ItemList>
+
+      <ItemList>
+        {`Design e desenvolvimento web: `}
+        <Link target="_blank" href={`mailto:${rodrigo}?subject=${subject}&body=${body}`}>
+          Rodrigo Avellar de Muniagurria
+        </Link>
+      </ItemList>
+
+      <ItemList>
+        {`Desenvolvimento web: `}
+        <Link target="_blank" href={`mailto:${gustavo}?subject=${subject}&body=${body}`}>
+          Gustavo Sohne
+        </Link>
+      </ItemList>
     </>
   )
 }
