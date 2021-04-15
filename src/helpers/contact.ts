18e3b5130b595d346b1d990a44a82bbd05a83c96
Rodrigo Avellar <roavellarm@gmail.com>
2021-04-14 22:52:01 -0300

    Update contacts

--- src/helpers/contact.ts
+++
@@ -1,7 +0,0 @@
-const contact = {
-  email: 'martabrietzke@gmail.com',
-  subject: 'Contato do site',
-  body: 'Olá! Eu sou <seu nome>...',
-}
-
-export default contact
---
+++ src/helpers/contacts.ts
@@ -0,0 +1,7 @@
+const contacts = {
+  gustavo: 'gustavosohne38@gmail.com',
+  marta: 'martabrietzke@gmail.com',
+  rodrigo: 'ramprofissional@gmail.com',
+}
+
+export default contacts
