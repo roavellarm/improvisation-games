fa57788bc3f4e05ba1262ee81a0b8c8dcb3bb58e
Rodrigo Avellar <roavellarm@gmail.com>
2021-06-27 21:09:11 -0300

    Ad quarantine-games route

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -4,6 +4,7 @@ import loadable from 'loadable-components'
 
 const About = loadable(() => import('pages/About'))
 const Article = loadable(() => import('pages/Article'))
+const QuarantineGames = loadable(() => import('pages/QuarantineGames'))
 const Game = loadable(() => import('pages/Game'))
 const Home = loadable(() => import('pages/Home'))
 const NotFound = loadable(() => import('pages/NotFound'))
@@ -16,6 +17,7 @@ export default function Routes() {
         <Route path="/game/:id" component={Game} />
         <Route path="/about" component={About} />
         <Route path="/article" component={Article} />
+        <Route path="/quarantine-games" component={QuarantineGames} />
         <Route path="*" component={NotFound} />
       </Switch>
     </BrowserRouter>
