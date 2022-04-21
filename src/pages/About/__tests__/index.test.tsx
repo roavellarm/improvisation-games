3449d04a4978f2fe8348c314cbaab5dab3a7d271
Gustavo <51864014+GustavoFSProg@users.noreply.github.com>
2022-04-21 02:11:44 -0300

    Change alunos to estudantes (#33)
    * change estudents
    
    * Update the rest of "alunos"
    
    * Fix lint issues
    
    Co-authored-by: Rodrigo Avellar <roavellarm@gmail.com>
    Co-authored-by: Rodrigo Avellar <18143403+roavellarm@users.noreply.github.com>

--- src/pages/About/__tests__/index.test.tsx
+++ src/pages/About/__tests__/index.test.tsx
@@ -36,12 +36,12 @@ describe('Section Knowledge content', () => {
 
     expect(
       queryByText(
-        'Este site foi pensado para professores de violoncelo de alunos iniciantes de desenvolvimento instrumental, que utilizam abordagens de ensino coletivo. É um produto de uma pesquisa de mestrado que se realizou na Universidade de São Paulo, São Paulo, Brasil, nos anos de 2017 e 2018.'
+        'Este site foi pensado para professores de violoncelo de estudantes iniciantes de desenvolvimento instrumental, que utilizam abordagens de ensino coletivo. É um produto de uma pesquisa de mestrado que se realizou na Universidade de São Paulo, São Paulo, Brasil, nos anos de 2017 e 2018.'
       )
     ).toBeInTheDocument()
     expect(
       queryByText(
-        'Apesar do trabalho de pesquisa ter sido direcionado para alunos de violoncelo, muitas das propostas contidas nos jogos podem ser desenvolvidas para outros instrumentos e, também, para outros níveis de aprendizagem, cabendo a cada professor fazer as adaptações que julgar necessárias e efetivas para suas demandas. Além disso, cabem muitos desdobramentos das propostas iniciais, que podem suscitar outras ideias e perspectivas de trabalho.'
+        'Apesar do trabalho de pesquisa ter sido direcionado para estudantes de violoncelo, muitas das propostas contidas nos jogos podem ser desenvolvidas para outros instrumentos e, também, para outros níveis de aprendizagem, cabendo a cada professor fazer as adaptações que julgar necessárias e efetivas para suas demandas. Além disso, cabem muitos desdobramentos das propostas iniciais, que podem suscitar outras ideias e perspectivas de trabalho.'
       )
     ).toBeInTheDocument()
     expect(queryByText('Boa diversão e boas aprendizagens!')).toBeInTheDocument()
@@ -53,7 +53,7 @@ describe('Section Knowledge content', () => {
     ).toBeInTheDocument()
     expect(
       queryByText(
-        'Atuei como instrumentista e como professora em orquestras e projetos sociais da capital e da região metropolitana do meu estado. Em conjunto com alguns alunos de violoncelo, fundamos em Porto Alegre um espaço autogestionado de ensino e aprendizagem do violoncelo, denominado Filarmônica de Violoncelos-Tchêllistas. Minha área de pesquisa se relaciona ao ensino coletivo de instrumentos musicais, abordando distintas possibilidades e facetas. Também me dedico à performance de música contemporânea de concerto, fazendo a estreia de algumas peças de compositores brasileiros.'
+        'Atuei como instrumentista e como professora em orquestras e projetos sociais da capital e da região metropolitana do meu estado. Em conjunto com alguns estudantes de violoncelo, fundamos em Porto Alegre um espaço autogestionado de ensino e aprendizagem do violoncelo, denominado Filarmônica de Violoncelos-Tchêllistas. Minha área de pesquisa se relaciona ao ensino coletivo de instrumentos musicais, abordando distintas possibilidades e facetas. Também me dedico à performance de música contemporânea de concerto, fazendo a estreia de algumas peças de compositores brasileiros.'
       )
     ).toBeInTheDocument()
   })
