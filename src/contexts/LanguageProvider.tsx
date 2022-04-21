b7fb7acf201ac74d0b9146a3cc8dcaa427390854
Gustavo <gustavo.prog40@gmail.com>
2022-04-21 13:20:39 -0300

    refactor language context

--- src/contexts/LanguageProvider.tsx
+++ src/contexts/LanguageProvider.tsx
@@ -2,41 +2,19 @@
 import React, { createContext, useState } from 'react'
 
 interface ContextData {
-  state: Object
-  setState: Object
-  onChangeLanguage: () => void
+  language: String
+  setLanguage: Object
 }
 
 export const LanguageContext = createContext<ContextData>({} as ContextData)
 // eslint-disable-next-line react-hooks/rules-of-hooks
 
 export const LanguageProvider: React.FC = ({ children }) => {
-  const initialState = {
-    language: 'pt-BR',
-    headerTitle: 'Reconhecimento de voz',
-    startButton: 'Começar',
-    stopButton: 'Parar',
-    clearButton: 'Limpar',
-    fieldTitle: 'Transcrição',
-  }
-  const [state, setState] = useState(initialState)
+  const [language, setLanguage] = useState('portuguese')
   // const useLanguage = useContext(LanguageContext)
 
-  const onChangeLanguage = () => {
-    if (state.language !== 'pt-BR') return setState(initialState)
-
-    return setState({
-      language: 'en-US',
-      headerTitle: 'Speech recognition',
-      startButton: 'Start',
-      stopButton: 'Stop',
-      clearButton: 'Clear',
-      fieldTitle: 'Transcription',
-    })
-  }
-
   return (
-    <LanguageContext.Provider value={{ state, setState, onChangeLanguage }}>
+    <LanguageContext.Provider value={{ language, setLanguage }}>
       {children}
     </LanguageContext.Provider>
   )
