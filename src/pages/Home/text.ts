49fa7d24ba67aefc9c467340fcc7cbad3d2d5557
Rodrigo Avellar <roavellarm@gmail.com>
2022-04-22 02:21:09 -0300

    Add english text

---
+++ src/pages/Home/text.ts
@@ -0,0 +1,23 @@
+export type TextType = {
+  title: string
+  subtitle: string
+  quarantineLink: string
+  aboutLink: string
+  articleLink: string
+}
+
+export const portuguese = {
+  title: 'Jogos de improvisação',
+  subtitle: 'para violoncelistas iniciantes',
+  quarantineLink: 'Sons da quarentena',
+  aboutLink: 'Saiba mais',
+  articleLink: 'Caderno de atividades',
+} as TextType
+
+export const english = {
+  title: 'Improvisation games',
+  subtitle: 'for beginner cellists',
+  quarantineLink: 'Quarantine sounds',
+  aboutLink: 'About',
+  articleLink: 'Article',
+} as TextType
