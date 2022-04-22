946f11d0cc1ac0fd95e05da255c5d7c1a090eaed
Rodrigo Avellar <roavellarm@gmail.com>
2022-04-22 02:21:38 -0300

    Update LanguageContext

--- src/contexts/LanguageContext.tsx
+++ src/contexts/LanguageContext.tsx
@@ -2,24 +2,27 @@ import React, { createContext, useContext, useState } from 'react'
 
 interface ContextData {
   language: string
-  selectLanguage(language: string): void // eslint-disable-line
+  selectPortuguese(): void
+  selectEnglish(): void
+  selectSpanish(): void
 }
 
 const LanguageContext = createContext<ContextData>({} as ContextData)
 
+const LANGUAGE_OPTIONS = {
+  pt: 'pt',
+  en: 'en',
+  es: 'es',
+}
+
 export const LanguageProvider = ({ children }: { children: React.ReactNode }) => {
-  const LANGUAGE_OPTIONS = {
-    pt: 'pt',
-    en: 'en',
-    es: 'es',
-  }
-
   const [language, setLanguage] = useState(LANGUAGE_OPTIONS.pt)
-
-  const selectLanguage = (lang: string) => setLanguage(lang)
+  const selectPortuguese = () => setLanguage(LANGUAGE_OPTIONS.pt)
+  const selectEnglish = () => setLanguage(LANGUAGE_OPTIONS.en)
+  const selectSpanish = () => setLanguage(LANGUAGE_OPTIONS.es)
 
   return (
-    <LanguageContext.Provider value={{ language, selectLanguage }}>
+    <LanguageContext.Provider value={{ language, selectPortuguese, selectEnglish, selectSpanish }}>
       {children}
     </LanguageContext.Provider>
   )
