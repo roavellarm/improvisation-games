0b70a08e06c60b7bba8c18cb759be865cb63723a
Rodrigo Avellar <roavellarm@gmail.com>
2022-05-09 22:02:51 -0300

    Add LanguageSelector component

---
+++ src/components/LanguageSelector/index.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import { useLanguage } from 'contexts/LanguageContext'
+import globeIcon from 'assets/images/globe-icon.svg'
+import ptIcon from 'assets/images/pt-icon.png'
+import enIcon from 'assets/images/en-icon.png'
+import esIcon from 'assets/images/es-icon.png'
+import { Wrapper, Button, DropdownContent, Options, Arrow } from './styles'
+
+export const LanguageSelector = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const { language } = useLanguage()
+
+  const openMenu = () => setIsOpen(true)
+  const closeMenu = () => setIsOpen(false)
+
+  const languages = [
+    { key: 'pt', label: 'Português', icon: ptIcon },
+    { key: 'en', label: 'English', icon: enIcon },
+    { key: 'es', label: 'Español', icon: esIcon },
+  ]
+
+  const getSelectedLanguage = () => languages.filter((lang) => lang.key === language && language)[0]
+
+  const renderCurrentLanguage = () => {
+    const currentLanguage = getSelectedLanguage()
+    return (
+      <>
+        {currentLanguage.key.toUpperCase()}
+        &nbsp;
+        <img src={globeIcon} height={20} width={20} alt="Globe icon" />
+        &nbsp;
+        <Arrow isOpen={isOpen} />
+      </>
+    )
+  }
+
+  return (
+    <Wrapper>
+      <Button onMouseEnter={openMenu} onMouseLeave={closeMenu} isOpen={isOpen}>
+        {renderCurrentLanguage()}
+      </Button>
+
+      {isOpen && (
+        <DropdownContent onMouseEnter={openMenu} onMouseLeave={closeMenu}>
+          {languages.map((lang) => (
+            <Options key={lang.key} value={lang.key} onClick={() => null}>
+              <img src={lang.icon} height={20} width={20} alt={`${lang.label} icon`} />
+              &nbsp;
+              {lang.label}
+            </Options>
+          ))}
+        </DropdownContent>
+      )}
+    </Wrapper>
+  )
+}
---
+++ src/components/LanguageSelector/styles.ts
@@ -0,0 +1,62 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  position: absolute;
+  right: 1rem;
+  top: 1rem;
+  @media screen and (max-width: 800px) {
+    top: 4px;
+  }
+`
+
+export const Button = styled.div<{ isOpen: boolean }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  color: ${({ theme, isOpen }) => (isOpen ? theme.colors.white : theme.colors.orange)};
+  border-radius: ${({ isOpen }) => (isOpen ? '8px 8px 0 0' : '8px')};
+  box-shadow: ${({ isOpen }) => (isOpen ? 'none' : '2px 2px 6px 1px rgba(91, 124, 107, 0.5)')};
+  background: ${({ theme, isOpen }) => (isOpen ? theme.colors.orange : 'none')};
+  padding: 3px 10px;
+  transition: all ease 0.25s;
+  &:hover {
+    background: ${({ theme }) => theme.colors.orange};
+    color: ${({ theme }) => theme.colors.white};
+    box-shadow: none;
+    border-radius: 8px 8px 0 0;
+  }
+`
+
+export const Arrow = styled.div<{ isOpen: boolean }>`
+  border: solid black;
+  border-width: 0 2px 2px 0;
+  display: inline-block;
+  padding: 2px;
+  border-radius: 1px;
+  margin-top: 6px;
+  transform: ${({ isOpen }) => (isOpen ? 'rotate(-135deg)' : 'rotate(45deg)')};
+  -webkit-transform: ${({ isOpen }) => (isOpen ? 'rotate(-135deg)' : 'rotate(45deg)')};
+`
+
+export const DropdownContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  background: rgba(219, 152, 19, 0.5);
+  border-radius: 0 0 8px 8px;
+`
+
+export const Options = styled.div<{ value: string }>`
+  display: flex;
+  align-items: center;
+  font-size: 1.2rem;
+  line-height: 160%;
+  width: 100%;
+  padding: 0.3rem 1rem;
+  cursor: pointer;
+  transition: all ease 0.3s;
+  &:hover {
+    background: rgba(91, 124, 107, 0.2);
+  }
+`
