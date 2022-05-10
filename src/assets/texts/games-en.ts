36da9af4c8bac4faad0b0014877faa53ce2899a3
Rodrigo Avellar <roavellarm@gmail.com>
2022-05-10 00:12:07 -0300

    Add games texts files

---
+++ src/assets/texts/games-en.ts
@@ -0,0 +1,14 @@
+import { Game } from 'types'
+
+export const gameListEn: Game[] = [
+  {
+    id: '1',
+    gameTitle: 'Game 1',
+    content: [
+      {
+        text: 'TODO',
+        style: ['title1'],
+      },
+    ],
+  },
+]
---
+++ src/assets/texts/games-es.ts
@@ -0,0 +1,14 @@
+import { Game } from 'types'
+
+export const gameListEs: Game[] = [
+  {
+    id: '1',
+    gameTitle: 'Juego 1',
+    content: [
+      {
+        text: '...',
+        style: ['title1'],
+      },
+    ],
+  },
+]
