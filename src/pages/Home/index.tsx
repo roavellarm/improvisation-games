4f7a145376e8d0bb27e0d35c475cdd8ef5c5ccf4
Rodrigo Avellar <roavellarm@gmail.com>
2022-05-21 20:11:16 -0300

    Update homepage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useLayoutEffect, useMemo, useState } from 'react'
+import React, { useCallback, useLayoutEffect, useState } from 'react'
 import { useHistory } from 'react-router-dom'
 
 import Button from 'components/Button'
@@ -6,6 +6,7 @@ import Carousel from 'components/Carousel'
 import { LanguageSelector } from 'components/LanguageSelector'
 import { useLanguage } from 'contexts/LanguageContext'
 import { GameIndex } from 'types'
+import { useScreenSize } from 'contexts/screenSize'
 import { english, portuguese, spanish } from './text'
 import * as S from './styles'
 
@@ -16,12 +17,10 @@ const TEXTS: any = {
 }
 
 export default function Home() {
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
   const [text, setText] = useState(portuguese)
   const { push } = useHistory()
   const { language } = useLanguage()
-
-  const isMobileScreen = useMemo(() => windowWidth <= 800, [windowWidth])
+  const { isMobile } = useScreenSize()
 
   const handleClick = (game: string) => push(`/game/${game}`)
 
@@ -47,26 +46,17 @@ export default function Home() {
     </>
   )
 
-  const updateWindowWidth = useCallback(() => {
-    setWindowWidth(window.innerWidth)
-  }, [])
-
   const handleLanguage = useCallback(() => setText(TEXTS[`${language}`]), [language])
 
   useLayoutEffect(() => {
     handleLanguage()
   }, [handleLanguage])
 
-  useEffect(() => {
-    window.addEventListener('resize', updateWindowWidth)
-    return () => window.removeEventListener('resize', updateWindowWidth)
-  }, [updateWindowWidth])
-
   return (
     <S.Wrapper>
       <LanguageSelector />
       <S.Container>
-        {isMobileScreen ? (
+        {isMobile ? (
           <>
             <S.TitlesContainer>{renderTitle()}</S.TitlesContainer>
             {renderImageWithLinks()}
