e7564c764ff00bee84ac838d5e439d388b9c52ea
Gustavo <gustavo.prog40@gmail.com>
2022-07-15 12:46:33 -0300

    juego 1

--- src/pages/Game/games-es.ts
+++ src/pages/Game/games-es.ts
@@ -8,8 +8,84 @@ export const gameListEs: Game[] = [
     gameTitle: 'Juego 1',
     content: [
       {
-        text: '...',
+        text: 'Pregunta y Respuesta con cordas al aire en pizzicato',
         style: ['title1'],
+      },
+      {
+        text: 'Autoría: Marta Brietzke',
+        style: ['paragraph'],
+      },
+      {
+        text: 'Objetivos:',
+        style: ['title2'],
+      },
+      {
+        text:
+          'Propiciar a los estudiantes la conciencia de su individualidad y su papel dentro del colectivo; establecer las ideas de narrativa musical e de preguntas y respuestas, enfatizando el uso de las cuatro cuerdas del violoncello, del pizzicato, de diferentes ritmos, métricas, timbres y dinámicas.',
+        style: ['paragraph'],
+      },
+      {
+        text: 'Metodología:',
+        style: ['title2'],
+      },
+      {
+        text: 'Organizar a los estudiantes en un semicírculo.',
+        style: ['itemList'],
+      },
+      {
+        text:
+          'Explicar a los estudiantes qué podría ser una pregunta y qué podría ser uan respuesta dentro de una narrativa musical.',
+        style: ['itemList'],
+      },
+      {
+        text:
+          'Ejemplificar utilizando a penas las cuerdas al aire del violoncello, contemplando diferentes ritmos, métricas, dinámicas y variaciones de carácter.',
+        style: ['itemList'],
+      },
+      {
+        text:
+          'Proponer una pregunta al primer estudiante, que debe tocar una respuesta. En seguida, hacer lo mismo en relación al segundo estudiante ,y así sucesivamente, abordando a todos los estudiantes, uno a uno, individualmente.',
+        style: ['itemList'],
+      },
+      {
+        text: 'Realizar algunas vueltas del juego.',
+        style: ['itemList'],
+      },
+      {
+        text: 'Organizar a los estudiantes en dúos, dispuestos en diferentes puntos de la sala.',
+        style: ['itemList'],
+      },
+      {
+        text:
+          'Un estudiante de cada dúo debe hacer las preguntas, y el otro estudiante debe hacer las respuestas durante algunos minutos.',
+        style: ['itemList'],
+      },
+      {
+        text: 'Alternar los roles de la actividad anterior.',
+        style: ['itemList'],
+      },
+      {
+        text: 'Organizar nuevamente al grupo en un semicírculo.',
+        style: ['itemList'],
+      },
+      {
+        text:
+          'Cada dúo va hacia el centro del semicírculo y realiza algunas de las series de preguntas y respuestas, presentándolas para sus compañeros de clase.',
+        style: ['itemList'],
+      },
+      {
+        text: 'Observaciones:',
+        style: ['title2'],
+      },
+      {
+        text:
+          'Proporcionar a los estudiantes autonomía de decisión en relación al número de preguntas y respuestas que ellos presentarán a sus compañeros, a la distribución de los roles y a las combinaciones entre ellos que determinen el inicio y el fin de la presentación para sus pares.',
+        style: ['itemList'],
+      },
+      {
+        text:
+          'Se observa, con frecuencia, un aumento de la complejidad en la elaboración de las preguntas y respuestas de los estudiantes a lo largo de las actividades. Es interesante enfatizar este hecho.',
+        style: ['itemList'],
       },
     ],
   },
