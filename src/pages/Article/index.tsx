df5042beca7c9730a670493346102e783a0cc562
Gustavo <gustavo.prog40@gmail.com>
2022-07-19 14:41:47 -0300

    refactor articles text es

--- src/pages/Article/index.tsx
+++ src/pages/Article/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ArticleLayout from 'components/Templates/ArticleLayout'
-import textData from './textData-PT'
+import textData from './textData'
 import { anchors } from './anchors'
 import { checkStepperPosition } from './checkStepperPosition'
 
