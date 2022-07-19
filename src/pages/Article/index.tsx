db111a58352a6b19871572777ed1fae353ef472e
Gustavo <gustavo.prog40@gmail.com>
2022-07-19 14:12:04 -0300

    articles es create and config

--- src/pages/Article/index.tsx
+++ src/pages/Article/index.tsx
@@ -1,14 +1,31 @@
 import React from 'react'
 import ArticleLayout from 'components/Templates/ArticleLayout'
-import textData from './textData'
+import { useLanguage } from 'contexts/LanguageContext'
+import textDataPt from './textData-PT'
+import textDataEs from './textData-ES'
 import { anchors } from './anchors'
+
 import { checkStepperPosition } from './checkStepperPosition'
 
+// const ARTICLE_LIST: any = {
+//   pt: textDataPt,
+//   en: textDataEs,
+// }
+
 export default function Article() {
+  const { language } = useLanguage()
+
+  function getSelectedGame() {
+    if (language === 'pt') return textDataPt
+    if (language === 'es') return textDataEs
+
+    return textDataPt
+  }
+
   return (
     <ArticleLayout
       headerTitle="Caderno de atividades"
-      textData={textData}
+      textData={getSelectedGame()}
       anchors={anchors}
       checkStepperPosition={checkStepperPosition}
     />
